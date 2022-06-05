4b26fb26a5d5540f90e697835967fda3d3f5373c
Fl4v10-F3L1X <flaviofrancisco1802@hotmail.com>
2022-06-05 19:03:03 +0100

    Add swiper interaction to review section

--- js/script.js
+++ js/script.js
@@ -48,6 +48,7 @@ var swiper = new Swiper(".product-slider", {
         delay: 7500,
         disableOnInteraction:false,
     },
+    centeredSlides: true,
     breakpoints: {
         0: {
             slidesPerView: 1,
@@ -61,4 +62,24 @@ var swiper = new Swiper(".product-slider", {
     },
 });    
     
+var swiper = new Swiper(".review-slider", {
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction:false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1020: {
+            slidesPerView: 3,
+        },
+    },
+});  
     
